536959dcb08eaea0e8684e7b160649833b5a18d9
Victoria <victoriaroan@gmail.com>
2019-12-09 17:38:29 -0500

    Chapter 7

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler, SMTPHandler
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -14,4 +18,38 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+
+# Logging
+if not app.debug:
+    # Email errors
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = () if app.config['MAIL_USE_TLS'] else None
+
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-replace@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            subject='Microblog Error',
+            credentials=auth,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    # Log to file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler(
+        'logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(file_handler)
+
+from app import errors, models, routes
+
+app.logger.setLevel(logging.INFO)
+app.logger.info('Microblog startup')
--- config.py
+++ config.py
@@ -6,8 +6,16 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # General Config
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    ADMINS = ['test@example.com']
 
     # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL') or 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Mail
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 25
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
