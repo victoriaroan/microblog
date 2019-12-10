44ab7ff472a6d2bcdd6cde77a3fd5a7981c8f34c
vgood <vgood@linode.com>
2019-12-10 17:24:34 -0500

    Chapter 11

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import os
 from logging.handlers import RotatingFileHandler, SMTPHandler
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -13,6 +14,7 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
